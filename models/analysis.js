b2ec1ee5908b40b68c5114c4b9511f5899d9f0b0
Rivadeneira Lichardi Lucas <lrlichardi@hotmail.com>
2022-05-03 22:19:28 -0300

    fix console.log

--- models/analysis.js
+++ models/analysis.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { getSequenceNextValue , insertCounter } = require("./Counter");
+
 const autoIncrement = require('mongoose-sequence')(mongoose);
 
 const AnalysisSchema = new Schema({
@@ -19,32 +19,7 @@ const AnalysisSchema = new Schema({
   date: {
     type: String,
   },
-},
-//  { _id: false }
-);
+});
 AnalysisSchema.plugin(autoIncrement);
-// AnalysisSchema.pre("save", (next) => {
-//   let doc = this;
-//   getSequenceNextValue("user_id")
-//   .then((counter) => {
-//     console.log('contador' , counter);
-//     if (!counter) {
-//          insertCounter("user_id")
-//         .then((counter) => {
-//           doc._id = counter;
-//           console.log('prueba ' , doc);
-//           next();
-//         })
-//         .catch((error) => next(error));
-//     } else {
-//       console.log('en else')
-//       doc._id = counter;
-//       next();
-//     }
-//   }).catch(error => {
-//     console.log(error)
-//     next(error)});
-  
-// });
 
 module.exports = mongoose.model("Analysis", AnalysisSchema);
